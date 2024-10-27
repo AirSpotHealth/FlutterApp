b7905d1e08daa48b65539c87fe1c0779eef55a55
Narendra Bhatt <narendra.bhatt@cclsolution.com>
2024-10-28 10:39:55 +1100

    added animation

--- lib/features/devices/widgets/device_value_widget.dart
+++ lib/features/devices/widgets/device_value_widget.dart
@@ -14,16 +14,31 @@ class DeviceValueWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final deviceValue = ref.watch(bleDeviceCommunicationProvider(deviceId));
 
-    debugPrint('DeviceValueWidget: $deviceValue');
-
-    return Text(
-      deviceValue != null
-          ? "CO${Constants.subscript2} $deviceValue ppm"
-          : '------',
-      style: context.textTheme.titleLarge?.copyWith(
-        color: AppUtils.getDataColorFromValue(deviceValue),
-        fontWeight: FontWeight.bold,
-      ),
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "CO${Constants.subscript2} ",
+          style: context.textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: AppUtils.getDataColorFromValue(deviceValue),
+          ),
+        ),
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return ScaleTransition(scale: animation, child: child);
+          },
+          child: Text(
+            deviceValue != null ? "$deviceValue ppm" : '------',
+            key: ValueKey<String>(deviceValue?.toString() ?? '------'),
+            style: context.textTheme.titleLarge?.copyWith(
+              color: AppUtils.getDataColorFromValue(deviceValue),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
