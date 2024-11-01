91d9933c2185f49a90251b4bdf63075232b1159f
Narendra Bhatt <narendra.bhatt@cclsolution.com>
2024-11-01 14:34:54 +1100

    Enhance SettingItemWidget to conditionally disable interactions and adjust visuals based on item.enabled property

--- lib/features/device_settings/widgets/setting_item_widget.dart
+++ lib/features/device_settings/widgets/setting_item_widget.dart
@@ -1,3 +1,4 @@
+import 'package:airspothealth/core/utils/extensions.dart';
 import 'package:airspothealth/features/device_settings/models/setting_item.dart';
 import 'package:flutter/material.dart';
 
@@ -15,13 +16,17 @@ class SettingItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: item.enabled ? onTap : null,
       child: Container(
         padding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
         color: Colors.white,
         child: Row(
           children: [
-            Image.asset(item.assetIcon, width: 32),
+            Image.asset(
+              item.assetIcon,
+              width: 32,
+              opacity: AlwaysStoppedAnimation(item.enabled ? 1.0 : 0.5),
+            ),
             const SizedBox(width: 16),
             Expanded(
               child: DecoratedBox(
@@ -38,11 +43,29 @@ class SettingItemWidget extends StatelessWidget {
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Expanded(flex: 8, child: Text(item.title)),
+                        Expanded(
+                          flex: 8,
+                          child: Text(
+                            item.title,
+                            style: context.textTheme.bodyMedium?.copyWith(
+                              color: item.enabled ? Colors.black : Colors.grey,
+                            ),
+                          ),
+                        ),
                         const SizedBox(width: 8),
-                        item.suffixWidget ??
-                            const Icon(Icons.arrow_forward_ios,
-                                size: 16, color: Colors.grey),
+                        if (!item.enabled)
+                          const Icon(
+                            Icons.lock,
+                            size: 16,
+                            color: Colors.grey,
+                          )
+                        else
+                          item.suffixWidget ??
+                              const Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                color: Colors.grey,
+                              ),
                       ],
                     ),
                     const SizedBox(height: 12),
