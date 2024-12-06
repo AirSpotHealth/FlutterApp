fc309f466e0a437a924803e8e6d1c87088e28b26
Narendra Bhatt <nirajbhatt1997@gmail.com>
2024-12-07 09:11:53 +1100

    Fix power mode command parameters in DeviceCmdUtils for correct functionality

--- lib/core/utils/device_cmd_utils.dart
+++ lib/core/utils/device_cmd_utils.dart
@@ -118,20 +118,20 @@ class DeviceCmdUtils {
 
   // ======= Power Mode Commands =======
 
-  static Uint8List setPowerOnDemand() {
-    return _buildCommand([prefixHigh, prefixLow, 5, 1, 0, 0xAF]);
-  }
+  // static Uint8List setPowerOnDemand() {
+  //   return _buildCommand([prefixHigh, prefixLow, 5, 1, 0, 0xAF]);
+  // }
 
   static Uint8List setPowerLow() {
-    return _buildCommand([prefixHigh, prefixLow, 5, 1, 1, 0xB0]);
+    return _buildCommand([prefixHigh, prefixLow, 5, 1, 0, 0xB0]);
   }
 
   static Uint8List setPowerMed() {
-    return _buildCommand([prefixHigh, prefixLow, 5, 1, 2, 0xB1]);
+    return _buildCommand([prefixHigh, prefixLow, 5, 1, 1, 0xB1]);
   }
 
   static Uint8List setPowerHi() {
-    return _buildCommand([prefixHigh, prefixLow, 5, 1, 3, 0xB2]);
+    return _buildCommand([prefixHigh, prefixLow, 5, 1, 2, 0xB2]);
   }
 
   static Uint8List openPowerMode() {
