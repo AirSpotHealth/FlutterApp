db15a779e24d152808faaa963e6bcf83c37ba46a
Narendra Bhatt <nirajbhatt1997@gmail.com>
2024-12-06 17:23:48 +1100

    Remove test parameter from versionCheck API endpoint

--- lib/core/utils/api_endpoints.dart
+++ lib/core/utils/api_endpoints.dart
@@ -1,3 +1,5 @@
 class ApiEndpoints {
-  static const String versionCheck = '/firmware?test=true';
+  static const String versionCheck = '/firmware';
+
+  // static const String versionCheck = '/firmware?test=true';
 }
