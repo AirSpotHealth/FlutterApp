0bca55e1f078d892a5c99accf2d87c8de94d233f
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-01-03 20:52:48 +1100

    Refactor disconnect method in BLEService for improved readability

--- lib/core/services/ble_service.dart
+++ lib/core/services/ble_service.dart
@@ -58,7 +58,7 @@ class BLEService {
       device.connect(autoConnect: true, mtu: null);
 
   /// Method to disconnect from a Bluetooth device.
-  Future<void> disconnect(BluetoothDevice device) async => device.disconnect();
+  Future<void> disconnect(BluetoothDevice device) => device.disconnect();
 
   /// Get the stream of isScanning.
   Stream<bool> get isScanning => FlutterBluePlus.isScanning;
