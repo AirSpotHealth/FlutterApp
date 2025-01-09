908b85389d96fa122c44503e18fadac2f87fba0b
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-01-09 21:50:33 +1100

    Refactor RecalibrateDevicePage to improve readability and enable auto calibration settings

--- lib/features/device_settings/pages/recalibrate_device_page.dart
+++ lib/features/device_settings/pages/recalibrate_device_page.dart
@@ -1,4 +1,6 @@
+import 'package:airspothealth/core/models/device_settings.dart';
 import 'package:airspothealth/core/providers/ble_device_communication_provider.dart';
+import 'package:airspothealth/core/providers/device_settings_provider.dart';
 import 'package:airspothealth/core/utils/assets.dart';
 import 'package:airspothealth/core/utils/device_cmd_utils.dart';
 import 'package:airspothealth/features/device_settings/providers/recalibration_time_provider.dart';
@@ -23,8 +25,8 @@ class RecalibrateDevicePage extends ConsumerWidget {
       }
     });
 
-    // final DeviceSettings deviceSettings =
-    //     ref.watch(deviceSettingsProvider(deviceId));
+    final DeviceSettings deviceSettings =
+        ref.watch(deviceSettingsProvider(deviceId));
 
     final int? recalibrationTime =
         ref.watch(recalibrationTimeProvider(deviceId));
@@ -48,9 +50,9 @@ class RecalibrateDevicePage extends ConsumerWidget {
                 },
                 child: Image.asset(Assets.recalibrateImage, height: 100)),
             const SizedBox(height: 100),
-            // const Text(
-            //     'The AirSpot device automatically calibrates itself to the lowest CO₂ levels it sees over a week.'),
-            // const SizedBox(height: 16),
+            const Text(
+                'The AirSpot device automatically calibrates itself to the lowest CO₂ levels it sees over a week.'),
+            const SizedBox(height: 16),
             const Text(
                 'If your AirSpot requires forced calibration then place it in a well-ventilated outdoor space, stand at least 1.5 meters away from it, and press the calibration icon above.'),
             const Spacer(),
@@ -70,20 +72,19 @@ class RecalibrateDevicePage extends ConsumerWidget {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 16),
-            ]
-            // else
-            //   SwitchListTile(
-            //     title: const Text('Auto Calibration',
-            //         style:
-            //             TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-            //     value: deviceSettings.autoCalibration,
-            //     onChanged: (bool value) {
-            //       ref
-            //           .read(deviceSettingsProvider(deviceId).notifier)
-            //           .updateSettings(
-            //               deviceSettings.copyWith(autoCalibration: value));
-            //     },
-            //   ),
+            ] else
+              SwitchListTile(
+                title: const Text('Auto Calibration',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                value: deviceSettings.autoCalibration,
+                onChanged: (bool value) {
+                  ref
+                      .read(deviceSettingsProvider(deviceId).notifier)
+                      .updateSettings(
+                          deviceSettings.copyWith(autoCalibration: value));
+                },
+              ),
           ],
         ),
       ),
