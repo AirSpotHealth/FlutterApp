bbfbc05aa3b485792e6d7f6f05636ea553b91292
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-01-17 00:26:17 +1100

    Add power off functionality and widget to device settings

---
+++ lib/features/device_settings/widgets/power_off_device_widget.dart
@@ -0,0 +1,56 @@
+import 'package:airspothealth/core/providers/ble_device_communication_provider.dart';
+import 'package:airspothealth/core/utils/assets.dart';
+import 'package:airspothealth/core/utils/device_cmd_utils.dart';
+import 'package:airspothealth/core/widgets/button.dart';
+import 'package:airspothealth/features/device_settings/models/setting_item.dart';
+import 'package:airspothealth/features/device_settings/widgets/setting_item_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PowerOffDeviceWidget extends ConsumerWidget {
+  const PowerOffDeviceWidget({required this.deviceId, super.key});
+
+  final String deviceId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SettingItemWidget(
+      item: SettingItem(
+        title: 'Power Off Device',
+        assetIcon: Assets.powerOff,
+        suffixWidget: const Icon(Icons.power_settings_new),
+      ),
+      onTap: () {
+        // show a dialog to confirm the power off action
+        showAdaptiveDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Power Off Device'),
+            content: const Text(
+                'Are you sure you want to power off the device? This will put the device into sleep mode and will only wake up when the button is pressed in the device.'),
+            actions: [
+              Button(
+                type: ButtonType.text,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+              Button(
+                type: ButtonType.text,
+                onPressed: () {
+                  ref
+                      .read(bleDeviceCommunicationProvider(deviceId).notifier)
+                      .sendCommand(DeviceCmdUtils.powerOff());
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Power Off'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
