7eabb5218f818f5a7a650759bedaecbe1371f0cc
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-01-21 00:57:35 +1100

    Refactor power off functionality to use a modal bottom sheet for confirmation

--- lib/features/device_settings/widgets/power_off_device_widget.dart
+++ lib/features/device_settings/widgets/power_off_device_widget.dart
@@ -1,6 +1,9 @@
 import 'package:airspothealth/core/providers/ble_device_communication_provider.dart';
+import 'package:airspothealth/core/theme/app_colors.dart';
 import 'package:airspothealth/core/utils/assets.dart';
 import 'package:airspothealth/core/utils/device_cmd_utils.dart';
+import 'package:airspothealth/core/utils/extensions.dart';
+import 'package:airspothealth/core/widgets/app_bottomsheet.dart';
 import 'package:airspothealth/core/widgets/button.dart';
 import 'package:airspothealth/features/device_settings/models/setting_item.dart';
 import 'package:airspothealth/features/device_settings/widgets/setting_item_widget.dart';
@@ -20,37 +23,59 @@ class PowerOffDeviceWidget extends ConsumerWidget {
         assetIcon: Assets.powerOff,
         suffixWidget: const SizedBox(),
       ),
-      onTap: () {
-        // show a dialog to confirm the power off action
-        showAdaptiveDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Power Off Device'),
-            content: const Text(
-                'Are you sure you want to power off the device? This will put the device into sleep mode and will only wake up when the button is pressed in the device.'),
-            actions: [
-              Button(
-                type: ButtonType.text,
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Cancel'),
+      onTap: () => _showPowerOffSheet(ref),
+    );
+  }
+
+  void _showPowerOffSheet(WidgetRef ref) {
+    showModalBottomSheet(
+      context: ref.context,
+      builder: (context) => AppBottomSheet(
+        child: Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 2, bottom: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Are you sure you want to power off the device?',
+                style: context.textTheme.bodyLarge?.weight600,
               ),
-              Button(
-                type: ButtonType.text,
-                onPressed: () {
-                  ref
-                      .read(bleDeviceCommunicationProvider(deviceId).notifier)
-                      .sendCommand(DeviceCmdUtils.powerOff());
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Power Off'),
+              const SizedBox(height: 16),
+              Text(
+                'This will put the device into sleep mode and you will need to press the button on the device to turn it back on. This is useful if you are not using the device for a long period of time and want to save battery.',
+                style: context.textTheme.bodyMedium,
               ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Button(
+                    wrapWidth: true,
+                    label: 'Cancel',
+                    type: ButtonType.text,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Button(
+                    wrapWidth: true,
+                    label: 'Power Off',
+                    backgroundColor: AppColors.brandColorRed,
+                    onPressed: () {
+                      ref
+                          .read(
+                              bleDeviceCommunicationProvider(deviceId).notifier)
+                          .sendCommand(DeviceCmdUtils.powerOff());
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              )
             ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
