7e6a57bb9b9385f16942cee1c4b6b96964fee1ad
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-01-28 21:41:19 +1100

    Replace AirspotBar with AppBar in DevicesPage and HomePage; update imports accordingly

--- lib/features/devices/devices_page.dart
+++ lib/features/devices/devices_page.dart
@@ -1,7 +1,8 @@
 import 'package:airspothealth/core/models/ble_device.dart';
 import 'package:airspothealth/core/providers/ble_saved_devices_provider.dart';
 import 'package:airspothealth/core/router/route_names.dart';
-import 'package:airspothealth/core/widgets/airspot_bar.dart';
+import 'package:airspothealth/core/theme/app_colors.dart';
+import 'package:airspothealth/core/widgets/app_logo.dart';
 import 'package:airspothealth/features/devices/widgets/ble_device_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,22 +16,51 @@ class DevicesPage extends ConsumerWidget {
     final List<BleDevice> savedDevicesList = ref.watch(bleSavedDevicesProvider);
 
     return Scaffold(
-      appBar: const AirspotBar(),
-      body: savedDevicesList.isEmpty
-          ? const Center(child: Text('No devices connected'))
-          : ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(height: 12),
-              itemCount: savedDevicesList.length,
-              padding: const EdgeInsets.all(16),
-              physics: AlwaysScrollableScrollPhysics(),
-              itemBuilder: (context, index) =>
-                  BleDeviceWidget(bleDevice: savedDevicesList[index]),
-            ),
-      floatingActionButton: FloatingActionButton.extended(
+        appBar: AppBar(
+          backgroundColor: AppColors.primaryColor,
+          title: const Padding(
+              padding: EdgeInsets.only(top: 12), child: AppLogo()),
+          centerTitle: true,
+          actions: [
+            if (savedDevicesList.length > 4)
+              GestureDetector(
+                onTap: () => context.pushNamed(RouteNames.addDevice),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.add, size: 20),
+                    const SizedBox(width: 2),
+                    const Text(
+                      'Add Device',
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            const SizedBox(width: 16),
+          ],
+        ),
+        body: savedDevicesList.isEmpty
+            ? const Center(child: Text('No devices connected'))
+            : ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 12),
+                itemCount: savedDevicesList.length,
+                padding: const EdgeInsets.all(16),
+                physics: AlwaysScrollableScrollPhysics(),
+                itemBuilder: (context, index) =>
+                    BleDeviceWidget(bleDevice: savedDevicesList[index]),
+              ),
+        floatingActionButton: savedDevicesList.length < 5
+            ? _buildAddDeviceButton(context)
+            : null);
+  }
+
+  FloatingActionButton _buildAddDeviceButton(BuildContext context) =>
+      FloatingActionButton.extended(
         label: const Text('Add Device'),
         icon: const Icon(Icons.add),
         onPressed: () => context.pushNamed(RouteNames.addDevice),
-      ),
-    );
-  }
+      );
 }
--- lib/features/home/homepage.dart
+++ lib/features/home/homepage.dart
@@ -1,5 +1,6 @@
+import 'package:airspothealth/core/theme/app_colors.dart';
 import 'package:airspothealth/core/utils/constants.dart';
-import 'package:airspothealth/core/widgets/airspot_bar.dart';
+import 'package:airspothealth/core/widgets/app_logo.dart';
 import 'package:airspothealth/features/add_device/providers/ble_search_results_provider.dart';
 import 'package:airspothealth/features/device_settings/providers/firmware_remote_version_provider.dart';
 import 'package:airspothealth/features/home/widgets/menu_item_widget.dart';
@@ -37,7 +38,12 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AirspotBar(),
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        title:
+            const Padding(padding: EdgeInsets.only(top: 12), child: AppLogo()),
+        centerTitle: true,
+      ),
       body: ListView.separated(
         padding: const EdgeInsets.all(16),
         separatorBuilder: (context, index) => const SizedBox(height: 12),
