06b27af43adaa54ac19d8786fc62d7970afd2f91
Narendra Bhatt <nirajbhatt1997@gmail.com>
2025-06-25 20:19:39 +1000

    Add CO2 Value Widget Extension and associated resources
    - Introduced a new CO2 Value Widget extension for monitoring CO2 levels from AirSpot devices.
    - Implemented the widget's data model, timeline provider, and UI components using SwiftUI.
    - Added necessary entitlements and Info.plist configurations for the widget extension.
    - Included asset catalogs for app icons and color sets to enhance the widget's appearance.
    - Updated project settings to integrate the new widget extension into the existing app structure.

---
+++ ios/Co2ValueWidget/Co2ValueWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Co2ValueWidgetBundle.swift
+//  Co2ValueWidget
+//
+//  Created by Niraj Bhatt on 25/06/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Co2ValueWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Co2ValueWidget()
+    }
+}
